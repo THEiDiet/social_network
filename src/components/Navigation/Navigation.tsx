2c1dc43df7ed39c022fd4091d29d1fc624bade36
THEiDiet <alex96kravets@gmail.com>
2021-11-30 01:24:36 +0700

    The beginning for my project on tsx

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation =() => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href={'#'}>Main</a></li>
+                <li><a href={'#'}>Messages</a></li>
+                <li><a href={'#'}>Friends</a></li>
+                <li><a href={'#'}>Settings</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
