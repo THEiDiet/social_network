9e25c811fce2780966e44a2d57db3ee0d2ee2759
THEiDiet <alex96kravets@gmail.com>
2021-11-30 14:12:15 +0700

    second branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Rating} from "./components/rating/Rating";
 import {Accordeon} from "./components/accordeon/Accordeon";
-import { Rating } from './components/rating/Rating';
 
 function App() {
-    console.log('App will be render')
     return (
-        <div className="App">
-            <AppTitle title={'Hello'}/>
-            <AppTitle title={'or hi'}/>
-            <Accordeon title={'Menu'} collapsed={true}/>
-            <Accordeon title={'Users'} collapsed={false}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
+        <div className="wrapper">
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Accordeon title={'Hey'} collapsed={true}/>
+            <Accordeon title={'Hi'} collapsed={false}/>
         </div>
-    )
-}
-type AppTitlePropsType = {
-    title:string
-}
-function AppTitle(props:AppTitlePropsType){
-    return (
-        <h1>{props.title}</h1>
     )
 }
 
