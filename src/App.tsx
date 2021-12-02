2b87eb2f0d64916ed9ab1730e6d682a3ba6fbeab
THEiDiet <alex96kravets@gmail.com>
2021-12-02 23:35:44 +0700

    Experiments with MUI

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import {CssBaseline} from "@mui/material";
 
 function App() {
     return (
-        <div className="wrapper">
-            <Header/>
-            <Navigation/>
-            <Main/>
-            <Footer/>
-        </div>
+        <>
+            <CssBaseline enableColorScheme/>
+            <div className="wrapper">
+                <Header/>
+                <Navigation/>
+
+                <Main/>
+                {/*<Footer/>*/}
+            </div>
+        </>
     )
 }
 
